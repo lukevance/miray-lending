4096db18f68be1b378be207f340acbbd0ad725d8
Luke Vance <lukevance90@gmail.com>
2016-03-18 10:13:04 -0600

    added partial for navbar and css

--- frontend/js/services/users.service.js
+++ frontend/js/services/users.service.js
@@ -2,10 +2,22 @@ angular.module('lendingApp')
   .service('newUserService', ['$http', newUserService]);
 
 function newUserService($http){
-  return function () {
-    $http.post()
-  }
-  // connect to new user post request at API
+  return function (userData, nextFunc) {
+    // connect to new user post request at API
+    $http.post('//localhost:3000', userData)
+    .then(function(responseData, err){
+      if (err) {
+        throw err;
+      } else {
+        console.log(responseData);
+        nextFunc(responseData);
+      }
+    })
+    .catch(function(err){
+      console.log(err);
+      // handle err
+    });
+  };
 }
 
 function editUserService ($http){
