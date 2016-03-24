76b0b18d7d72c63a10d1fac741b4c05e9ae6b358
Luke Vance <lukevance90@gmail.com>
2016-03-23 20:01:25 -0600

    navbar is now directive so it can be modified based on current user info

---
+++ frontend/js/directives/Navbar.directive.js
@@ -0,0 +1,28 @@
+angular.module('lendingApp')
+  .directive('navbarMiray', ['userSigninService', navbarMiray]);
+
+function navbarMiray (userSigninService) {
+  return  {
+    restrict: 'E',
+    scope: {
+      user: '='
+    },
+    templateUrl: 'partials/navbar.html',
+    controller: function ($scope, $window) {
+      if ($window.sessionStorage.profile) {
+        $scope.userInfo = $window.sessionStorage.profile;
+        console.log($scope.userInfo.name);
+      }
+      $scope.signinSubmit = signinSubmit;
+      function signinSubmit (email, password) {
+        var userCred = {email: email, password: password};
+        userSigninService(userCred, function(response){
+          console.log(response);
+          $window.sessionStorage.profile = response.data.profile;
+          $window.sessionStorage.token = response.data.token;
+        });
+      }
+
+    }
+  };
+}
