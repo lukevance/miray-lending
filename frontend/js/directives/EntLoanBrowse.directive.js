8202b25f4df2e7a506b1b5a5b3013f9f04372e7e
Luke Vance <lukevance90@gmail.com>
2016-03-25 10:51:23 -0600

    added donation form to ent browse modal

--- frontend/js/directives/EntLoanBrowse.directive.js
+++ frontend/js/directives/EntLoanBrowse.directive.js
@@ -12,9 +12,20 @@ function entBrowse () {
       // remove this line later when donations are connected
       $scope.entrepreneur.amount_raised = 50;
       $scope.selectEnt = selectEnt;
+      $scope.signinActive = false;
+      $scope.donationActive = false;
       function selectEnt (clickedEnt) {
         console.log(clickedEnt);
         $scope.currEnt = clickedEnt;
+      }
+
+      $scope.donate = donate;
+      function donate () {
+        // if user not signed in
+        $scope.signinActive = false;
+        // else open donation
+        $scope.donationActive = true;
+
       }
     }
   };
