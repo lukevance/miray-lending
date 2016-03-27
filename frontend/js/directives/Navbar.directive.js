b797ba8b916e0a0c771efe128f48e8b68fff78b6
Luke Vance <lukevance90@gmail.com>
2016-03-26 23:05:33 -0600

    navbar shows name of user and user menu if token is found

--- frontend/js/directives/Navbar.directive.js
+++ frontend/js/directives/Navbar.directive.js
@@ -9,11 +9,12 @@ function navbarMiray (userSigninService) {
     },
     templateUrl: 'partials/navbar.html',
     controller: function ($scope, $window) {
-      // if ($window.sessionStorage.profile) {
-      //   $scope.userInfo = $window.sessionStorage.profile;
-      //   console.log($scope.userInfo.name);
-      // }
+      if ($window.localStorage.token) {
+        $scope.userInfo = JSON.parse(window.atob($window.localStorage.token.split('.')[1]));
+        console.log(typeof $scope.userInfo);
+      }
       $scope.signinSubmit = signinSubmit;
+      // define signin function
       function signinSubmit (email, password) {
         var userCred = {email: email, password: password};
         userSigninService(userCred, function(response){
@@ -21,6 +22,11 @@ function navbarMiray (userSigninService) {
           // console.log(window.atob(response.data.token.split('.')[1]));
           $window.localStorage.token = response.data.token;
         });
+      }
+      $scope.signout = signout;
+      // define signout function
+      function signout () {
+        delete $window.localStorage.token;
       }
 
     }
