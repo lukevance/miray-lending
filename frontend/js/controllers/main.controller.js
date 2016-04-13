7a98beed77fa5515cfdf7d10881dd68d00668220
Luke Vance <lukevance90@gmail.com>
2016-04-13 10:14:57 -0600

    added pages for discover and how-it-works

--- frontend/js/controllers/main.controller.js
+++ frontend/js/controllers/main.controller.js
@@ -5,7 +5,6 @@ function MainController ($document, getEntrepreneursService) {
   var vm = this;
   vm.title = 'Miray Development';
   vm.subtitle = 'Micro Loans With Massive Impact';
-  vm.introParagraph = 'The primary purpose of the app would be as a fundraising tool. It would allow visitors to browse entrepreneurs currently in the pipeline and fund their loans. These loans are held for a maximum of 1 year, and many are paid back within months. So, this app would track repayment progress and allow "investors" to see their loan repaid with interest. Upon repayment the user would have anywhere from 100-130% of their original "investment" to re-invest in another entrepreneur (or multiple).';
 
   getEntrepreneursService(saveEntrepreneurs);
 
--- frontend/js/routes.js
+++ frontend/js/routes.js
@@ -21,13 +21,17 @@ angular.module('lendingApp')
         templateUrl: 'views/userProfile.html',
         controller: 'UserController as UC'
       })
-      .when('/backers', {
-        templateUrl: 'views/backers.html',
-        controller: 'BackersController as BC'
+      .when('/discover', {
+        templateUrl: 'views/discover.html',
+        controller: 'DiscoverController as DC'
       })
       .when('/admin', {
         templateUrl: 'views/admin.html',
         controller: 'AdminController as ADC'
+      })
+      .when('/how-it-works', {
+        templateUrl: 'views/how-it-works.html',
+        controller: 'AboutController as ABC'
       })
       .when('/404', {
         templateUrl: 'views/404.html',
